7f1e4ea15abaddcf55573d0ec9551f36f32ebaa2
BLannoo <brunolannoo@gmail.com>
2018-12-18 23:55:52 +0100

    day17 basic example till end, but no stop condition

--- solutions/day17/day17.py
+++ solutions/day17/day17.py
@@ -1,21 +1,9 @@
 import re
-import unittest
-from typing import List, Any
+from typing import List
 
 from solutions.utils.utils import grid_to_str, Location
 
 SOURCE_X = 500
-
-EXAMPLE_INPUT = """
-x=495, y=2..7
-y=7, x=495..501
-x=501, y=3..7
-x=498, y=2..4
-x=506, y=1..2
-x=498, y=10..13
-x=504, y=10..13
-y=13, x=498..504
-""".strip()
 
 
 def parse_vein(vein_string: str) -> List[Location]:
@@ -46,29 +34,44 @@ class Reservoir:
         self.__focus = Location(SOURCE_X - min_x, 0)
         self.__focuses_to_retry = []
 
-    def flow(self, i):
-        for _ in range(i):
+    def flow(self, i: int):
+        for temp in range(i):
             self.__flow()
 
     def __flow(self):
         if self.__grid[self.__focus.y + 1][self.__focus.x] != '.':
-            print('space bellow taken: ', Location(self.__focus.x, self.__focus.y + 1))
+            print('space bellow (', self.__focus, ') is taken')
             if self.__grid[self.__focus.y][self.__focus.x - 1] != '.':
-                print('space left taken: ', Location(self.__focus.x - 1, self.__focus.y))
-                self.__focus = self.__focuses_to_retry.pop()  # take the previous one
-                print('returning to: ', self.__focus)
-                self.__flow()
+                print('space left (', self.__focus, ') is taken')
+                if self.__grid[self.__focus.y][self.__focus.x + 1] != '.':
+                    print('space right (', self.__focus, ') is taken')
+                    self.__focus = self.__focuses_to_retry.pop()  # take the previous one
+                    print('returning to: ', self.__focus)
+                    self.__flow()
+                else:
+                    self.__change_focus(Location(self.__focus.x + 1, self.__focus.y))
             else:
                 self.__change_focus(Location(self.__focus.x - 1, self.__focus.y))
         else:
             self.__change_focus(Location(self.__focus.x, self.__focus.y + 1))
+
         self.__grid[self.__focus.y][self.__focus.x] = '|'
 
-    def __change_focus(self, location):
+        if self.__focus.y == len(self.__grid) - 1:
+            top_of_fall = self.remove_water_fall(self.__focus)
+            self.__focus = self.__focuses_to_retry.pop()
+
+    def remove_water_fall(self, bottom_of_fall: Location) -> Location:
+        top_of_fall = bottom_of_fall
+        while bottom_of_fall.x == self.__focuses_to_retry[-1].x:
+            top_of_fall = self.__focuses_to_retry.pop()
+        return top_of_fall
+
+    def __change_focus(self, location: Location):
         self.__focuses_to_retry.append(self.__focus)
         self.__focus = location
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return grid_to_str(self.__grid)
 
 
@@ -93,263 +96,3 @@ def scan_input(input_string: str) -> Reservoir:
     grid[0][SOURCE_X - min_x] = '+'
 
     return Reservoir(min_x, grid)
-
-
-class TestDay17(unittest.TestCase):
-    def test_render_vertical_vein(self):
-        self.assertEqual(
-            str(scan_input("x=495, y=2..7")),
-            """
-            .....+
-            ......
-            #.....
-            #.....
-            #.....
-            #.....
-            #.....
-            #.....
-            """.strip().replace(" ", "")
-        )
-
-    def test_render_horizontal_vein(self):
-        self.assertEqual(
-            str(scan_input("y=7, x=495..501")),
-            """
-            .....+.
-            .......
-            .......
-            .......
-            .......
-            .......
-            .......
-            #######
-            """.strip().replace(" ", "")
-        )
-
-    def test_render_example(self):
-        self.assertEqual(
-            str(scan_input(EXAMPLE_INPUT)),
-            """
-            .....+......
-            ...........#
-            #..#.......#
-            #..#..#.....
-            #..#..#.....
-            #.....#.....
-            #.....#.....
-            #######.....
-            ............
-            ............
-            ...#.....#..
-            ...#.....#..
-            ...#.....#..
-            ...#######..
-            """.strip().replace(' ', '')
-        )
-
-    def test_flow_step_1(self):
-        reservoir: Reservoir = scan_input(EXAMPLE_INPUT)
-        reservoir.flow(1)
-        self.assertEqual(
-            str(reservoir),
-            """
-            .....+......
-            .....|.....#
-            #..#.......#
-            #..#..#.....
-            #..#..#.....
-            #.....#.....
-            #.....#.....
-            #######.....
-            ............
-            ............
-            ...#.....#..
-            ...#.....#..
-            ...#.....#..
-            ...#######..
-            """.strip().replace(' ', '')
-        )
-
-    def test_flow_step_2(self):
-        reservoir: Reservoir = scan_input(EXAMPLE_INPUT)
-        reservoir.flow(2)
-        self.assertEqual(
-            str(reservoir),
-            """
-            .....+......
-            .....|.....#
-            #..#.|.....#
-            #..#..#.....
-            #..#..#.....
-            #.....#.....
-            #.....#.....
-            #######.....
-            ............
-            ............
-            ...#.....#..
-            ...#.....#..
-            ...#.....#..
-            ...#######..
-            """.strip().replace(' ', '')
-        )
-
-    def test_flow_step_6(self):
-        reservoir: Reservoir = scan_input(EXAMPLE_INPUT)
-        reservoir.flow(6)
-        self.assertEqual(
-            str(reservoir),
-            """
-            .....+......
-            .....|.....#
-            #..#.|.....#
-            #..#.|#.....
-            #..#.|#.....
-            #....|#.....
-            #....|#.....
-            #######.....
-            ............
-            ............
-            ...#.....#..
-            ...#.....#..
-            ...#.....#..
-            ...#######..
-            """.strip().replace(' ', '')
-        )
-
-    def test_flow_step_7(self):
-        reservoir: Reservoir = scan_input(EXAMPLE_INPUT)
-        reservoir.flow(7)
-        self.assertEqual(
-            str(reservoir),
-            """
-            .....+......
-            .....|.....#
-            #..#.|.....#
-            #..#.|#.....
-            #..#.|#.....
-            #....|#.....
-            #...||#.....
-            #######.....
-            ............
-            ............
-            ...#.....#..
-            ...#.....#..
-            ...#.....#..
-            ...#######..
-            """.strip().replace(' ', '')
-        )
-
-    def test_flow_step_10(self):
-        reservoir: Reservoir = scan_input(EXAMPLE_INPUT)
-        reservoir.flow(10)
-        self.assertEqual(
-            str(reservoir),
-            """
-            .....+......
-            .....|.....#
-            #..#.|.....#
-            #..#.|#.....
-            #..#.|#.....
-            #....|#.....
-            #|||||#.....
-            #######.....
-            ............
-            ............
-            ...#.....#..
-            ...#.....#..
-            ...#.....#..
-            ...#######..
-            """.strip().replace(' ', '')
-        )
-
-    def test_flow_step_11(self):
-        reservoir: Reservoir = scan_input(EXAMPLE_INPUT)
-        reservoir.flow(11)
-        self.assertEqual(
-            str(reservoir),
-            """
-            .....+......
-            .....|.....#
-            #..#.|.....#
-            #..#.|#.....
-            #..#.|#.....
-            #...||#.....
-            #|||||#.....
-            #######.....
-            ............
-            ............
-            ...#.....#..
-            ...#.....#..
-            ...#.....#..
-            ...#######..
-            """.strip().replace(' ', '')
-        )
-
-    def test_flow_step_12(self):
-        reservoir: Reservoir = scan_input(EXAMPLE_INPUT)
-        reservoir.flow(12)
-        self.assertEqual(
-            str(reservoir),
-            """
-            .....+......
-            .....|.....#
-            #..#.|.....#
-            #..#.|#.....
-            #..#.|#.....
-            #..|||#.....
-            #|||||#.....
-            #######.....
-            ............
-            ............
-            ...#.....#..
-            ...#.....#..
-            ...#.....#..
-            ...#######..
-            """.strip().replace(' ', '')
-        )
-
-    def test_flow_step_14(self):
-        reservoir: Reservoir = scan_input(EXAMPLE_INPUT)
-        reservoir.flow(14)
-        self.assertEqual(
-            str(reservoir),
-            """
-            .....+......
-            .....|.....#
-            #..#.|.....#
-            #..#.|#.....
-            #..#.|#.....
-            #|||||#.....
-            #|||||#.....
-            #######.....
-            ............
-            ............
-            ...#.....#..
-            ...#.....#..
-            ...#.....#..
-            ...#######..
-            """.strip().replace(' ', '')
-        )
-
-    def test_flow_step_15(self):
-        reservoir: Reservoir = scan_input(EXAMPLE_INPUT)
-        reservoir.flow(15)
-        self.assertEqual(
-            str(reservoir),
-            """
-            .....+......
-            .....|.....#
-            #..#.|.....#
-            #..#.|#.....
-            #..#||#.....
-            #|||||#.....
-            #|||||#.....
-            #######.....
-            ............
-            ............
-            ...#.....#..
-            ...#.....#..
-            ...#.....#..
-            ...#######..
-            """.strip().replace(' ', '')
-        )
